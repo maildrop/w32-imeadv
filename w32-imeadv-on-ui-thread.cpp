b8aec9a48e060836ac3a19fb583c1f7d8ab0bd10
TOGURO Mikito <mit@shalab.net>
2018-11-27 19:35:54 +0900

    メッセージクラックをif else でバグを入れ込んだので switch で実行するようにする
    else if ( WM_W32_IMEADV_NULL == uMsg ) でメッセージクラックしていたのだが
    これを
    else if ( WM_W32_IMEADV_NULL )
    と、比較を忘れるというミスを犯したので、心を入れ替えて switch - case でメッセージクラックすることにする。

--- w32-imeadv-on-ui-thread.cpp
+++ w32-imeadv-on-ui-thread.cpp
@@ -13,6 +13,15 @@ static LRESULT
 w32_imm_wm_ime_composition( HWND hWnd , WPARAM wParam , LPARAM lParam );
 static LRESULT
 w32_imm_wm_ime_endcomposition( HWND hWnd , WPARAM wParam , LPARAM lParam );
+static LRESULT
+w32_imm_wm_ime_notify( HWND hWnd, WPARAM wParam ,LPARAM  lParam );
+static LRESULT
+w32_imeadv_null( HWND hWnd , WPARAM wParam , LPARAM lParam );
+static LRESULT
+w32_imeadv_openstatus_open( HWND hWnd , WPARAM wParam , LPARAM lParam );
+static LRESULT
+w32_imeadv_openstatus_close( HWND hWnd , WPARAM wParam , LPARAM lParam );
+
 
 static LRESULT
 w32_imm_wm_ime_startcomposition( HWND hWnd , WPARAM wParam , LPARAM lParam )
@@ -95,7 +104,8 @@ w32_imm_wm_ime_endcomposition( HWND hWnd , WPARAM wParam , LPARAM lParam )
 static LRESULT
 w32_imm_wm_ime_notify( HWND hWnd, WPARAM wParam ,LPARAM  lParam )
 {
-  OutputDebugStringA( "w32_imm_wm_ime_notify( HWND hWnd, WPARAM wParam ,LPARAM  lParam )\n");
+  // TODO
+  //OutputDebugStringA( "w32_imm_wm_ime_notify( HWND hWnd, WPARAM wParam ,LPARAM  lParam )\n");
 
   switch( wParam ){
   case IMN_SETOPENSTATUS:
@@ -127,7 +137,6 @@ w32_imeadv_null( HWND hWnd , WPARAM wParam , LPARAM lParam )
 {
   return 1;
 }
-
 
 static LRESULT
 w32_imeadv_openstatus_open( HWND hWnd , WPARAM wParam , LPARAM lParam )
@@ -161,7 +170,8 @@ LRESULT (CALLBACK subclass_proc)( HWND hWnd , UINT uMsg , WPARAM wParam , LPARAM
   if( WM_DESTROY == uMsg )
     SendMessage( hWnd, WM_W32_IMEADV_UNSUBCLASSIFY , 0 , 0 ); // 自分自身に送る
 
-  if( WM_KEYDOWN == uMsg )
+  switch( uMsg ){
+  case WM_KEYDOWN:
     {
       switch( wParam ){
       case VK_PROCESSKEY:
@@ -179,30 +189,34 @@ LRESULT (CALLBACK subclass_proc)( HWND hWnd , UINT uMsg , WPARAM wParam , LPARAM
         break;
       }
     }
-  else if ( WM_IME_COMPOSITION == uMsg )
+    return ::DefSubclassProc( hWnd , uMsg , wParam ,lParam );
+  case WM_IME_COMPOSITION:
     return w32_imm_wm_ime_composition( hWnd , wParam , lParam );
-
-  else if ( WM_IME_STARTCOMPOSITION == uMsg )
+  case WM_IME_STARTCOMPOSITION:
     return w32_imm_wm_ime_startcomposition( hWnd ,wParam , lParam);
-
-  else if ( WM_IME_ENDCOMPOSITION == uMsg )
+  case WM_IME_ENDCOMPOSITION:
     return w32_imm_wm_ime_endcomposition( hWnd, wParam , lParam );
-
-  else if ( WM_IME_NOTIFY == uMsg )
+  case WM_IME_NOTIFY :
     return w32_imm_wm_ime_notify( hWnd, wParam , lParam );
-
-  else if ( WM_W32_IMEADV_NULL == uMsg )
+    
+    /* ********************************** */
+    /* Private Window Message             */
+    /* ********************************** */
+  case WM_W32_IMEADV_NULL:
     return w32_imeadv_null( hWnd , wParam , lParam );
-
-  else if ( WM_W32_IMEADV_OPENSTATUS_OPEN == uMsg)
+  case WM_W32_IMEADV_OPENSTATUS_OPEN:
     return w32_imeadv_openstatus_open( hWnd , wParam , lParam );
-
-  else if ( WM_W32_IMEADV_OPENSTATUS_CLOSE == uMsg )
+  case WM_W32_IMEADV_OPENSTATUS_CLOSE:
     return w32_imeadv_openstatus_close( hWnd , wParam , lParam );
+    
+  default: 
+    break;
+  }
 
-  else if ( WM_W32_IMEADV_SUBCLASSIFY == uMsg )
+  if ( WM_W32_IMEADV_SUBCLASSIFY == uMsg )
     {
-
+      // これは、ウィンドウを開いたときに IME が動作しないことがあるという問題に対応するため
+      // 一旦ウィンドウを開けたり閉じたりする必要がある。 どうしてなのかはわからない。
       HIMC hImc = ImmGetContext( hWnd );
       if( hImc )
         {
@@ -218,10 +232,9 @@ LRESULT (CALLBACK subclass_proc)( HWND hWnd , UINT uMsg , WPARAM wParam , LPARAM
         }
       
       HWND communication_window_handle = (HWND)(wParam);
-      
       SetProp( hWnd , "W32_IMM32ADV_COMWIN" , (communication_window_handle ) );
-      
       PostMessageA( communication_window_handle , WM_W32_IMEADV_SUBCLASSIFY , (WPARAM)( hWnd ) , 0);
+      return 1;
     }
   else if( WM_W32_IMEADV_UNSUBCLASSIFY == uMsg )
     {
@@ -232,5 +245,6 @@ LRESULT (CALLBACK subclass_proc)( HWND hWnd , UINT uMsg , WPARAM wParam , LPARAM
       }
       return ::RemoveWindowSubclass( hWnd , subclass_proc , uIdSubclass);
     }
+  
   return DefSubclassProc (hWnd, uMsg , wParam , lParam );
 }
