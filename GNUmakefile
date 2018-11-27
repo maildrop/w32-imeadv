7bc0b1bbafadcaeb0c6aab5fc151ee054f45c116
TOGURO Mikito <mit@shalab.net>
2018-11-27 11:45:20 +0900

    emacs-imm32-input-proxy と名前をつけた通知用のプログラムを追加

--- GNUmakefile
+++ GNUmakefile
@@ -1,11 +1,14 @@
 CPPFLAGS:=-DWINVER=_WIN32_WINNT_WINXP -D_WIN32_WINNT=_WIN32_WINNT_WINXP -DCHECK_LISP_OBJECT_TYPE=1
 CXXFLAGS:=-std=c++11 -O3 -g -Wall
-
 W32_IMEADV_DLL_OBJS:=w32-imeadv.o w32-imeadv-on-lispy-thread.o w32-imeadv-on-ui-thread.o
+emacs-imm32-input-proxy_OBJS:= emacs-imm32-input-proxy.o
+
+all: w32-imeadv.dll emacs-imm32-input-proxy.exe
 
 w32-imeadv.dll: $(W32_IMEADV_DLL_OBJS)
 	g++ --shared -o $@ $(CXXFLAGS) $(CPPFLAGS) $+ -limm32 -lComctl32 -lUser32
-
+emacs-imm32-input-proxy.exe: $(emacs-imm32-input-proxy_OBJS)
+	g++ -o $@ $(CXXFLAGS) $(CPPFLAGS) $+ -lUser32
 
 w32-imeadv.o: w32-imeadv.cpp emacs-module.h w32-imeadv.h w32-imeadv-on-lispy-thread.h 
 
@@ -13,5 +16,7 @@ w32-imeadv-on-lispy-thread.o: w32-imeadv-on-lispy-thread.cpp w32-imeadv.h w32-im
 
 w32-imeadv-on-ui-thread.o: w32-imeadv-on-ui-thread.cpp w32-imeadv.h 
 
+emacs-imm32-input-proxy.o: emacs-imm32-input-proxy.cpp w32-imeadv.h
+
 clean:
-	rm -f w32-imeadv.dll $(W32_IMEADV_DLL_OBJS) *~
+	rm -f w32-imeadv.dll $(W32_IMEADV_DLL_OBJS) $(emacs-imm32-input-proxy_OBJS) *~
---
+++ emacs-imm32-input-proxy.cpp
@@ -0,0 +1,109 @@
+﻿#include <tchar.h>
+#include <windows.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+#include "w32-imeadv.h"
+
+#ifdef _MSC_VER
+#pragma comment(lib,"user32.lib")
+#endif /* defined( _MSC_VER ) */
+
+static int
+notify_output( int c )
+{
+  putchar( c );
+  fflush( stdout );
+  return 1; 
+}
+
+static LRESULT
+(CALLBACK wnd_proc)( HWND hWnd , UINT uMsg , WPARAM wParam, LPARAM lParam );
+
+static LRESULT
+(CALLBACK wnd_proc)( HWND hWnd , UINT uMsg , WPARAM wParam, LPARAM lParam )
+{
+  if( WM_DESTROY == uMsg ){
+    PostQuitMessage(0);
+  }
+  switch( uMsg ){
+  case WM_W32_IMEADV_END:
+    return DestroyWindow( hWnd );
+  case WM_W32_IMEADV_OPENSTATUS_OPEN :
+    return notify_output( '1' );
+  case WM_W32_IMEADV_OPENSTATUS_CLOSE :
+    return notify_output( '0' );
+  case WM_W32_IMEADV_REQUEST_RECONVERSION_STRING:
+    return notify_output( 'R' );
+  case WM_W32_IMEADV_REQUEST_DOCUMENTFEED_STRING:
+    return notify_output( 'D' );
+  default:
+    break;
+  }
+  return ::DefWindowProc( hWnd , uMsg , wParam , lParam );
+}
+
+int main(int argc,char* argv[])
+{
+  if( 1 < argc ){
+    
+    HWND hWnd = reinterpret_cast<HWND>(static_cast<intptr_t>( atoi( argv[1] )));
+    if( hWnd ){
+      std::stringstream out{};
+      out << reinterpret_cast<intptr_t>(hWnd) << std::endl;
+      OutputDebugStringA( out.str().c_str() );
+    }
+  }
+  
+  HINSTANCE hInstance = GetModuleHandle( NULL );
+
+  WNDCLASSEX wndClassEx    = {};
+  {
+    wndClassEx.cbSize        = sizeof( WNDCLASSEX );
+    wndClassEx.style         = 0;
+    wndClassEx.lpfnWndProc   = wnd_proc;
+    wndClassEx.cbClsExtra    = 0;
+    wndClassEx.cbWndExtra    = 0;
+    wndClassEx.hInstance     = hInstance;
+    wndClassEx.hIcon         = 0;
+    wndClassEx.hCursor       = LoadCursor(nullptr, IDC_ARROW);
+    wndClassEx.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
+    wndClassEx.lpszMenuName  = 0;
+    wndClassEx.lpszClassName = "EmacsIMM32EventProxyWindowClassA";
+    wndClassEx.hIconSm       = 0;
+  }
+  
+  ATOM windowAtom  = ::RegisterClassExA( &wndClassEx );
+  if( windowAtom ){
+    HWND hWnd = 
+      CreateWindowExA(0,reinterpret_cast<LPCSTR>( windowAtom ) ,
+                      "EmacsIMM32EventProxyWindow",
+                      WS_OVERLAPPEDWINDOW,
+                      CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+                      HWND_MESSAGE, NULL, hInstance, nullptr);    
+    if( hWnd ){
+      PostMessageA( hWnd , WM_W32_IMEADV_OPENSTATUS_OPEN , 0 , 0 );
+      PostMessageA( hWnd , WM_W32_IMEADV_OPENSTATUS_CLOSE , 0 , 0 );
+      PostMessageA( hWnd , WM_W32_IMEADV_REQUEST_DOCUMENTFEED_STRING , 0 , 0 );
+      PostMessageA( hWnd , WM_W32_IMEADV_REQUEST_RECONVERSION_STRING , 0 , 0 );
+      PostMessageA( hWnd , WM_W32_IMEADV_END , 0 ,0  );
+      for(;;){
+        MSG msg = {0};
+        switch( GetMessageA( &msg , NULL , 0 , 0 ) ){
+        case -1:
+        case 0:
+          goto end_of_message_pump;
+        default:
+          TranslateMessage( &msg );
+          DispatchMessageA( &msg );
+        }
+      }
+    end_of_message_pump:
+      ;
+    }
+    UnregisterClass( reinterpret_cast<LPCTSTR>( windowAtom ) , hInstance);
+  }
+
+  return EXIT_SUCCESS;
+}
