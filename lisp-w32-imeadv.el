3c7045723724f94f275f05c6ccdaaa02df4c3b95
TOGURO Mikito <mit@shalab.net>
2018-12-24 18:44:25 +0900

    mule-cmds.el のしきたりに合わせて、 register-input-method をするように変更
    register-input-method 用に w32-imeadv-state-switch メソッドを提供することにより C-\ でIME のオンオフが出来るようになった

--- lisp-w32-imeadv.el
+++ lisp-w32-imeadv.el
@@ -3,23 +3,26 @@
 ;; w32-imeadv 初期化部分
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(when (and window-system (locate-library "w32-imeadv") ) ; Windowシステムであり、w32-imeadvが存在していれば、
+(when (and (eq system-type 'windows-nt)   ; Windows NT 上で
+           window-system                  ; Window システムがあって
+           (locate-library "w32-imeadv")) ; w32-imeadvが存在していれば、
   (load "w32-imeadv") ; w32-imeadv をロードする。
+  
   (when (w32-imeadv-initialize) ; w32-imeadv-initialize は失敗することがあります。
     ; 通知用のサブプロセス( UIスレッドのイベントを、self-pipe-trick で、入力へ変換する ) の起動
-    (let ((process-connection-type nil )
-          (process-adaptive-read-buffering nil)
-          (process-name "emacs-imm32-input-proxy"))
-	  (start-process process-name nil
-                     "rundll32.exe" 
-                     (format "%s" (w32-imeadv--get-module-filename))
+    (let ( (process-connection-type nil )        ; pipe を使います
+           (process-adaptive-read-buffering nil) ; adaprive である必要はありません
+           (process-name "emacs-imm32-input-proxy") )
+      (start-process process-name nil
+                     "rundll32.exe"
+                     (w32-imeadv--get-module-filename)
                      "EntryPoint"
                      (number-to-string (w32-imeadv--get-communication-hwnd))) 
-	  (set-process-filter (get-process process-name) 'w32-imeadv--defualt-message-input-handler )
+      (set-process-filter (get-process process-name) 'w32-imeadv--defualt-message-input-handler )
       (set-process-query-on-exit-flag (get-process process-name) nil)
       (add-hook 'kill-emacs-hook (lambda () (when (process-live-p (get-process "emacs-imm32-input-proxy"))
                                               (delete-process "emacs-imm32-input-proxy") t) )))
-
+    
     ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
     ;; IME Composition フォントの設定
     ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -32,7 +35,7 @@
                (if (and (boundp 'w32-imeadv-ime-composition-font-attributes)
                         (not (null w32-imeadv-ime-composition-font-attributes )))
                    w32-imeadv-ime-composition-font-attributes
-                 (font-face-attributes (face-font 'default nil (char-before))))) )
+                 (font-face-attributes (face-font 'default nil ?あ )))) ) ; ?あ or (char-before)
         ;; TODO 本来はここで run-hook して、フォントの調整をする機会をユーザーに与えるべきである。
         (w32-imeadv-advertise-ime-composition-font-internal w32-imeadv-ime-composition-font-attributes )
         ))
@@ -43,12 +46,15 @@
       (w32-imeadv-set-openstatus-close (string-to-number (frame-parameter (selected-frame) 'window-id))))
     (add-hook 'isearch-mode-hook 'w32-imeadv-default-isearch-hook )
 
+    ;; ステータスラインの設定 
     (defvar w32-imeadv-status-line-format (list "[　]" "[あ]") )
     (defvar w32-imeadv-status-line (nth 0 w32-imeadv-status-line-format) )
+    
     (defun w32-imeadv-status-line-show ()
       "Get a string to be displayed on the mode-line."
       (format "%s" w32-imeadv-status-line ))
     (setq-default mode-line-format (cons '(:eval (w32-imeadv-status-line-show)) mode-line-format))
+
     ;; IME が off になったときに呼ばれるフック関数
     (add-hook 'w32-imeadv-ime-off-hook
               (lambda ()
@@ -69,7 +75,20 @@
     ;; 今は、init.el の中なので、最初のフレームに対して、 w32-imeadv-inject-control する。
     ;; selected-frame（現在のフレーム）にw32-imeadvを導入する
     (w32-imeadv-inject-control (string-to-number (frame-parameter (selected-frame)'window-id)))
-    
+    (defun w32-imeadv-state-switch ( &optional arg )
+      "w32-imeadv-state-switch method"
+      (if arg
+          (progn
+            (setq deactivate-current-input-method-function 'w32-imeadv-state-switch)
+            (run-hooks 'input-method-activate-hook)
+            (setq describe-current-input-method-function nil)
+            (w32-imeadv-set-openstatus-open (string-to-number (frame-parameter (selected-frame) 'window-id))))
+        (progn
+          (run-hooks 'input-method-deactivate-hook)
+          (setq describe-current-input-method-function nil)
+          (w32-imeadv-set-openstatus-close (string-to-number (frame-parameter (selected-frame) 'window-id))))))
+    ;; mule-cmds.el の input-method に W32-IMEADV を登録する
+    (register-input-method "W32-IMEADV" "Japanese" 'w32-imeadv-state-switch "" "W32 System IME" )
     )) ;; end of initialize w32-imeadv
 
 ;;;;;;;;;;;;;;;;; w32-imeadv の初期化ここまで ;;;;;;;;;;;;;;;;;;;
@@ -95,4 +114,7 @@
                 ; スタックから元のカーソル色を取得して、カーソルの色を変更
                 (let ( (cursol-corlor (pop w32-imeadv-ime-on-hook-color-stack)) )
                   (when cursol-corlor
-                    (set-cursor-color cursol-corlor ))) ))) )
+                    (set-cursor-color cursol-corlor ))) )))
+
+  ;; 最後にdefault-input-method を W32-IMEADV にする。(これ重要)
+  (setq default-input-method "W32-IMEADV") )
